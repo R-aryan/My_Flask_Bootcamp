dab9e481d9c5fc22f0d66dc1aac22ff678599ef6
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2019-06-14 13:16:00 +0530

    added files

--- 02-Python/Python-Level one Basics/06-Exercise_Review_SOLUTIONS.py
+++ 02-Python/Python-Level one Basics/06-Exercise_Review_SOLUTIONS.py
@@ -1,6 +1,6 @@
-#####################################
-#### PART 6: EXERCISE REVIEW  #######
-#####################################
+##############################################
+#### PART 6: EXERCISE REVIEW SOLUTIONS #######
+##############################################
 
 # Time to review all the basic data types we learned! This should be a
 # relatively straight-forward and quick assignment.
@@ -14,17 +14,18 @@ s = 'flask'
 
 # Use indexing to print out the following:
 # 'f'
-
+s[0]
 # 's'
-
+s[3]
 # 'ask'
-
+s[2:]
 # 'las'
-
+s[1:4]
 # 'k'
-
+s[-1]
+#or
+s[4]
 # Bonus: Use indexing to reverse the string
-
 
 ###############
 ## Problem 2 ##
@@ -33,7 +34,7 @@ s = 'flask'
 # Given this nested list:
 mylist = [3,7,[1,4,'hello']]
 # Reassign "hello" to be "goodbye"
-
+mylist[2][2] = "goodbye"
 
 ###############
 ## Problem 3 ##
@@ -43,10 +44,15 @@ mylist = [3,7,[1,4,'hello']]
 
 d1 = {'simple_key':'hello'}
 
+d1['simple_key']
+
 d2 = {'k1':{'k2':'hello'}}
+
+d2['k1']['k2']
 
 d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
 
+d3['k1'][0]['nest_key'][1][0]
 
 ###############
 ## Problem 4 ##
@@ -54,7 +60,7 @@ d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
 
 # Use a set to find the unique values of the list below:
 mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
-
+set(mylist)
 
 ###############
 ## Problem 5 ##
@@ -66,3 +72,5 @@ name = "Sammy"
 
 # Use print formatting to print the following string:
 "Hello my dog's name is Sammy and he is 4 years old"
+
+print("Hello my dog's name is {} and he is {} years old".format(age,name))
