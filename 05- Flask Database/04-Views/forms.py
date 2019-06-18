1b0b5ccbe8a5ac466be826a098fcc9e964da168c
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2019-06-18 13:04:49 +0530

    added forms

---
+++ 05- Flask Database/04-Views/forms.py
@@ -0,0 +1,20 @@
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
+
+class Addform(FlaskForm):
+
+    name=StringField(' Enter Name of the Puppy :')
+    breed= StringField(' Enter the breed of the puppy :')
+    age= IntegerField(' Enter the age of the puppy :')
+    submit=SubmitField('Add Puppy')
+
+
+class Delform(FlaskForm):
+    
+    id= IntegerField('Enter the id of the puppy to remove')
+    submit= SubmitField('Remove Puppy')
+
+   
+    
+
